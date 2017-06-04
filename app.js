9c4b5c4e4d353fff8e1a0cf269a5c2c4d133e270
vjekooo <vjeko.pavicin@gmail.com>
2017-06-04 18:55:55 +0200

    Saving articles to MongoDB added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/node_articles');
 let db = mongoose.connection;
@@ -23,6 +24,12 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body-parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 // Home route
 app.get('/', (req, res) => {
   Article.find({}, (err, articles) => {
@@ -41,6 +48,23 @@ app.get('/', (req, res) => {
 app.get('/articles/add', (req, res) => {
   res.render('add_article', {
     title: 'Add article'
+  });
+});
+
+// Add submit post route
+app.post('/articles/add', (req, res) => {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save((err) => {
+    if(err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
