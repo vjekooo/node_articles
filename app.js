c5e6901a11dd8707cd535bf3764b57797736e79b
vjekooo <vjeko.pavicin@gmail.com>
2017-06-04 11:39:52 +0200

    Added pug templating engine

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home route
 app.get('/', (req, res) => {
-  res.send('Hello Noice');
+  res.render('index', {
+    title: 'Articles'
+  });
 });
 
+// Start server
 app.listen(3000, () => {
-  console.log('Express server live on port 3000');
+  console.log('Server started on port 3000');
 });
