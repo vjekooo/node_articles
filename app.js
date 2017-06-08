d4816c57220f2a0d72691dcb8f972b34f09a4438
vjekooo <vjeko.pavicin@gmail.com>
2017-06-08 22:14:29 +0200

    Update articles added

--- app.js
+++ app.js
@@ -80,6 +80,35 @@ app.post('/articles/add', (req, res) => {
   });
 });
 
+// Load edit form
+app.get('/article/edit/:id', (req,res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('edit_article', {
+      title: 'Edit article',
+      article: article
+    });
+  });
+});
+
+// Update submit post route
+app.post('/articles/edit/:id', (req, res) => {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id};
+
+  Article.update(query, article, (err) => {
+    if(err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
 // 404
 app.use((req, res) => {
   res.sendStatus(404);
